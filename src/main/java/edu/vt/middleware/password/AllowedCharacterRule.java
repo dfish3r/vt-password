5e993883fc94568ea65b0292e2f284f11815e80f
Daniel Fisher <dfisher@vt.edu>
2011-07-01 22:21:57 +0000

    Provide implementation of a rule which only allows a defined set of characters. See vt-password 115.

---
+++ src/main/java/edu/vt/middleware/password/AllowedCharacterRule.java
@@ -0,0 +1,97 @@
+/*
+  $Id$
+
+  Copyright (C) 2003-2011 Virginia Tech.
+  All rights reserved.
+
+  SEE LICENSE FOR MORE INFORMATION
+
+  Author:  Middleware Services
+  Email:   middleware@vt.edu
+  Version: $Revision$
+  Updated: $Date$
+*/
+package edu.vt.middleware.password;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Rule for determining if a password contains allowed characters.
+ *
+ * @author  Middleware Services
+ * @version  $Revision$ $Date$
+ */
+public class AllowedCharacterRule implements Rule
+{
+
+  /** Error code for allowed character failures. */
+  public static final String ERROR_CODE = "ALLOWED_CHAR";
+
+  /** Stores the characters that are allowed. */
+  private char[] allowedChar;
+
+
+  /**
+   * Create a new allowed character rule.
+   *
+   * @param  c  allowed characters
+   */
+  public AllowedCharacterRule(final char[] c)
+  {
+    allowedChar = c;
+    Arrays.sort(allowedChar);
+  }
+
+
+  /** {@inheritDoc} */
+  @Override
+  public RuleResult validate(final PasswordData passwordData)
+  {
+    final RuleResult result = new RuleResult(true);
+
+    for (char c : passwordData.getPassword().getText().toCharArray()) {
+      if (Arrays.binarySearch(allowedChar, c) < 0) {
+        result.setValid(false);
+        result.getDetails().add(
+          new RuleResultDetail(
+            ERROR_CODE, createRuleResultDetailParameters(c)));
+        break;
+      }
+    }
+    return result;
+  }
+
+
+  /**
+   * Creates the parameter data for the rule result detail.
+   *
+   * @param  c  illegal character
+   *
+   * @return  map of parameter name to value
+   */
+  protected Map<String, ?> createRuleResultDetailParameters(final char c)
+  {
+    final Map<String, Object> m = new LinkedHashMap<String, Object>();
+    m.put("illegalCharacter", c);
+    return m;
+  }
+
+
+  /**
+   * Returns a string representation of this object.
+   *
+   * @return  string representation
+   */
+  @Override
+  public String toString()
+  {
+    return
+      String.format(
+        "%s@%h::allowedChar=%s",
+        getClass().getName(),
+        hashCode(),
+        allowedChar != null ? Arrays.asList(allowedChar) : null);
+  }
+}
---
+++ src/test/java/edu/vt/middleware/password/AllowedCharacterRuleTest.java
@@ -0,0 +1,72 @@
+/*
+  $Id$
+
+  Copyright (C) 2003-2011 Virginia Tech.
+  All rights reserved.
+
+  SEE LICENSE FOR MORE INFORMATION
+
+  Author:  Middleware Services
+  Email:   middleware@vt.edu
+  Version: $Revision$
+  Updated: $Date$
+*/
+package edu.vt.middleware.password;
+
+import org.testng.AssertJUnit;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for {@link AllowedCharacterRule}.
+ *
+ * @author  Middleware Services
+ * @version  $Revision$
+ */
+public class AllowedCharacterRuleTest extends AbstractRuleTest
+{
+
+  /** Test password. */
+  private static final Password VALID_PASS = new Password("boepselwezz");
+
+  /** Test password. */
+  private static final Password INVALID_PASS = new Password("gbwersco4kk");
+
+  /** For testing. */
+  private AllowedCharacterRule rule = new AllowedCharacterRule(
+    new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', });
+
+
+  /**
+   * @return  Test data.
+   *
+   * @throws  Exception  On test data generation failure.
+   */
+  @DataProvider(name = "passwords")
+  public Object[][] passwords()
+    throws Exception
+  {
+    return
+      new Object[][] {
+
+        {rule, new PasswordData(VALID_PASS), true, },
+        {rule, new PasswordData(INVALID_PASS), false, },
+      };
+  }
+
+
+  /** @throws  Exception  On test failure. */
+  @Test(groups = {"passtest"})
+  public void resolveMessage()
+    throws Exception
+  {
+    final RuleResult result = rule.validate(new PasswordData(INVALID_PASS));
+    for (RuleResultDetail detail : result.getDetails()) {
+      AssertJUnit.assertEquals(
+        String.format("Password contains the illegal character '%s'.", "4"),
+        DEFAULT_RESOLVER.resolve(detail));
+      AssertJUnit.assertNotNull(EMPTY_RESOLVER.resolve(detail));
+    }
+  }
+}
