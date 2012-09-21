c7fc6657d6c809b65f4ac78c54d8497b4b0f21c1
Daniel Fisher <dfisher@vt.edu>
2012-09-21 16:03:33 +0000

    INSUFFICIENT_CHARACTERISTICS message property is incorrect, the reference to the number of rules and the number of required rules is reversed. Add unit test for this scenerio. Fixes vt-password 138.

--- src/test/java/edu/vt/middleware/password/CharacterCharacteristicsRuleTest.java
+++ src/test/java/edu/vt/middleware/password/CharacterCharacteristicsRuleTest.java
@@ -46,7 +46,11 @@ public class CharacterCharacteristicsRuleTest extends AbstractRuleTest
   private static final Password NONALPHA_PASS = new Password("r5scvEW2e9b");
 
   /** For testing. */
-  private CharacterCharacteristicsRule rule =
+  private CharacterCharacteristicsRule rule1 =
+    new CharacterCharacteristicsRule();
+
+  /** For testing. */
+  private CharacterCharacteristicsRule rule2 =
     new CharacterCharacteristicsRule();
 
 
@@ -54,12 +58,18 @@ public class CharacterCharacteristicsRuleTest extends AbstractRuleTest
   @BeforeClass(groups = {"passtest"})
   public void createRules()
   {
-    rule.getRules().add(new AlphabeticalCharacterRule(4));
-    rule.getRules().add(new DigitCharacterRule(3));
-    rule.getRules().add(new UppercaseCharacterRule(2));
-    rule.getRules().add(new LowercaseCharacterRule(2));
-    rule.getRules().add(new NonAlphanumericCharacterRule(1));
-    rule.setNumberOfCharacteristics(5);
+    rule1.getRules().add(new AlphabeticalCharacterRule(4));
+    rule1.getRules().add(new DigitCharacterRule(3));
+    rule1.getRules().add(new UppercaseCharacterRule(2));
+    rule1.getRules().add(new LowercaseCharacterRule(2));
+    rule1.getRules().add(new NonAlphanumericCharacterRule(1));
+    rule1.setNumberOfCharacteristics(5);
+
+    rule2.getRules().add(new DigitCharacterRule(1));
+    rule2.getRules().add(new NonAlphanumericCharacterRule(1));
+    rule2.getRules().add(new UppercaseCharacterRule(1));
+    rule2.getRules().add(new LowercaseCharacterRule(1));
+    rule2.setNumberOfCharacteristics(3);
   }
 
 
@@ -75,9 +85,9 @@ public class CharacterCharacteristicsRuleTest extends AbstractRuleTest
     return
       new Object[][] {
 
-        {rule, new PasswordData(VALID_PASS), null, },
+        {rule1, new PasswordData(VALID_PASS), null, },
         {
-          rule,
+          rule1,
           new PasswordData(ALPHA_PASS),
           codes(
             CharacterCharacteristicsRule.ERROR_CODE,
@@ -85,33 +95,39 @@ public class CharacterCharacteristicsRuleTest extends AbstractRuleTest
             LowercaseCharacterRule.ERROR_CODE),
         },
         {
-          rule,
+          rule1,
           new PasswordData(DIGIT_PASS),
           codes(
             CharacterCharacteristicsRule.ERROR_CODE,
             DigitCharacterRule.ERROR_CODE),
         },
         {
-          rule,
+          rule1,
           new PasswordData(UPPERCASE_PASS),
           codes(
             CharacterCharacteristicsRule.ERROR_CODE,
             UppercaseCharacterRule.ERROR_CODE),
         },
         {
-          rule,
+          rule1,
           new PasswordData(LOWERCASE_PASS),
           codes(
             CharacterCharacteristicsRule.ERROR_CODE,
             LowercaseCharacterRule.ERROR_CODE),
         },
         {
-          rule,
+          rule1,
           new PasswordData(NONALPHA_PASS),
           codes(
             CharacterCharacteristicsRule.ERROR_CODE,
             NonAlphanumericCharacterRule.ERROR_CODE),
         },
+        {rule2, new PasswordData(VALID_PASS), null, },
+        {rule2, new PasswordData(ALPHA_PASS), null, },
+        {rule2, new PasswordData(DIGIT_PASS), null, },
+        {rule2, new PasswordData(UPPERCASE_PASS), null, },
+        {rule2, new PasswordData(LOWERCASE_PASS), null, },
+        {rule2, new PasswordData(NONALPHA_PASS), null, },
       };
   }
 
@@ -136,7 +152,8 @@ public class CharacterCharacteristicsRuleTest extends AbstractRuleTest
   public void resolveMessage()
     throws Exception
   {
-    final RuleResult result = rule.validate(
+    // check messages for rule1
+    RuleResult result = rule1.validate(
       new PasswordData(new Password("r%scvEW2e3)")));
     for (int i = 0; i < result.getDetails().size(); i++) {
       final RuleResultDetail detail = result.getDetails().get(i);
@@ -164,6 +181,47 @@ public class CharacterCharacteristicsRuleTest extends AbstractRuleTest
       default:
         AssertJUnit.fail("Invalid index");
         break;
+      }
+      AssertJUnit.assertNotNull(EMPTY_RESOLVER.resolve(detail));
+    }
+
+    // check messages for rule2
+    result = rule2.validate(new PasswordData(new Password("rscvew2e3")));
+    for (int i = 0; i < result.getDetails().size(); i++) {
+      final RuleResultDetail detail = result.getDetails().get(i);
+      switch (i) {
+
+        case 0:
+          AssertJUnit.assertEquals(
+            String.format(
+              "Password must contain at least %s %s characters.",
+              1,
+              "non-alphanumeric"),
+            DEFAULT_RESOLVER.resolve(detail));
+          break;
+
+        case 1:
+          AssertJUnit.assertEquals(
+            String.format(
+              "Password must contain at least %s %s characters.",
+              1,
+              "uppercase"),
+            DEFAULT_RESOLVER.resolve(detail));
+          break;
+
+        case 2:
+          AssertJUnit.assertEquals(
+            String.format(
+              "Password matches %s of %s character rules, but %s are required.",
+              2,
+              4,
+              3),
+            DEFAULT_RESOLVER.resolve(detail));
+          break;
+
+        default:
+          AssertJUnit.fail("Invalid index");
+          break;
       }
       AssertJUnit.assertNotNull(EMPTY_RESOLVER.resolve(detail));
     }
